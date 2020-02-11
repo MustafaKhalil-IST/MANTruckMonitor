8830fd64dfdc8257dcd3c6ffd1507c90aa696f25
MustafaKhalil-IST <mustafa.samir.khalil@tecnico.ulisboa.pt>
2020-02-11 23:26:54 +0000

    added api doc swagger

--- TruckMonitor/backend/backend/src/main/java/org/man/truckmonitor/backend/api/TruckController.java
+++ TruckMonitor/backend/backend/src/main/java/org/man/truckmonitor/backend/api/TruckController.java
@@ -10,8 +10,12 @@ import org.man.truckmonitor.backend.service.TruckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
+@Api(description = "Set of endpoints for Trucks and Points of interests")
 public class TruckController {
     @Autowired
     private TruckService truckService;
@@ -21,25 +25,29 @@ public class TruckController {
 
     @CrossOrigin
     @GetMapping("/api/trucks/{plate}")
+    @ApiOperation("get truck by plate license")
     public TruckDTO getTruckByPlate(@PathVariable String plate) throws TruckException {
         return truckService.getTruckByPlate(plate);
     }
 
     @CrossOrigin
     @PostMapping("/api/trucks")
+    @ApiOperation("create truck")
     public TruckDTO createTruck(@RequestBody TruckDTO truckDTO) throws TruckException {
         return truckService.createTruck(truckDTO);
     }
 
     @CrossOrigin
     @PostMapping("/api/pois")
-    public JsonNode getPOIs(@RequestBody POISearchDTO poiSearchDTO) {
+    @ApiOperation("search points of interests")
+    public JsonNode searchPOIs(@RequestBody POISearchDTO poiSearchDTO) {
         return poIService.getPOIs(poiSearchDTO);
     }
 
 
     @CrossOrigin
     @PostMapping("/api/trucks/{plate}")
+    @ApiOperation("update truck location")
     public TruckLocationDTO updateTruckLocation(@PathVariable String plate, @Valid @RequestBody TruckLocationDTO truckLocationDTO) throws TruckException {
         return truckService.updateTruckLocation(truckLocationDTO, plate);
     }
